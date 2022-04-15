09bcd3048798358b9c6782344da4435e4b1fb68b
chayan-DUET <chayan_cse10@yahoo.com>
2022-04-15 11:05:35 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+/*import logo from './logo.svg';
+import './App.css'; */
+import React from "react";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
+/* import Dashboard from "./components/admin/Dashboard";
+import Profile from "./components/admin/Profile"; */
+
+import MasterLayout from "./layouts/admin/MasterLayout";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+        <Route path="/admin" name="Admin" render={(props) => <MasterLayout {...props} />} />
+          {/* <Route path="/admin/dashboard" element={<MasterLayout />} /> */}
+          {/* <Route exact path="/admin/dashboard" element={<Dashboard />} />
+          <Route exact path="/admin/profile" element={<Profile />} /> */}
+          {/* <Route path="/admin/dashboard" component={MasterLayout} /> */}
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/layouts/admin/MasterLayout.js
@@ -0,0 +1,45 @@
+import React from "react";
+import {Route, Redirect, Switch } from "react-router-dom";
+import '../../assets/admin/css/styles.css'
+import '../../assets/admin/js/scripts';
+import Footer from "./Footer";
+import Navebar from './Navbar';
+import Sidebar from "./Sidebar";
+import routes from "../../routes/routes";
+const MasterLayout = () =>{
+    return (
+        <div className="sb-nav-fixed">
+            <Navebar/>
+            <div id="layoutSidenav">
+                <div id="layoutSidenav_nav">
+                    <Sidebar/>
+                </div> 
+                <div id="layoutSidenav_content">
+                    <main>
+                    <Switch>
+                            {routes.map((route, index) =>{
+                                return(
+                                    route.component && (
+                                        <Route
+                                        key={index}
+                                        path={route.path}
+                                        exact={route.exact}
+                                        name={route.name}
+                                        render={(props) => (
+                                            <route.component {...props}/>
+                                        )}
+                                        />
+                                    )
+                                )
+                            })}
+                            <Redirect from ="admin" to="/admin/dashboard" />
+                            
+                    </Switch>
+                    </main>
+                    <Footer/>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default MasterLayout;
